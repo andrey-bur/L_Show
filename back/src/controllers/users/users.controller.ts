14a7c08d44114ab7539fb92354506635fb9bac4e
Akulich <kirillakulic1403@gmail.com>
2026-03-16 13:34:48 +0300

    Controller

--- back/src/controllers/users/users.controller.ts
+++ back/src/controllers/users/users.controller.ts
@@ -1,3 +1,39 @@
-export const Users=(req:Request,res:Response)=>{
-    
+import { readData, addItem, updateItem } from "../../servires/database.servires"
+
+const file = "users"
+
+type User = {
+  id: number
+  name: string
+  email: string
+  number: string
+  password: string
+}
+
+export async function findUser(firstName: string, lastName: string, password: string): Promise<User | null> {
+  const users: User[] = await readData(file)
+
+  const fullName = `${firstName} ${lastName}`
+
+  const user = users.find(
+    u => u.name == fullName && u.password == password
+  )
+
+  if (!user) {
+    return null
+  }
+
+  return user
+}
+
+export async function createUser(user: User): Promise<void> {
+  await addItem(file, user)
+}
+
+export async function updateUser(id: number, data: Partial<User>): Promise<void> {
+  const user = await updateItem(file, id, data)
+
+  if (!user) {
+
+  }
 }
